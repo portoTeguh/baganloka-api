6b53f8dd0f523bc23bd0eae6515dee224ce5057e
portoTeguh <teguh.krniawan1998@gmail.com>
2023-01-05 09:42:49 +0700

    api hotel done

--- routes/hotels.js
+++ routes/hotels.js
@@ -16,4 +16,47 @@ router.post('/', async (req, res) => {
     }
 })
 
+// update
+router.put('/:id', async (req, res) => {
+    try {
+        const updateHotel = await Hotel.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, {new: true})
+        res.status(200).json(updateHotel); 
+    } catch (error) {
+        res.status(500).json(error) 
+    }
+})
+
+// delete
+router.delete('/:id', async (req, res) => {
+    try {
+        await Hotel.findByIdAndDelete(req.params.id)
+        res.status(200).json('Hotel has been deleted'); 
+    } catch (error) {
+        res.status(500).json(error) 
+    }
+})
+
+// get detail
+router.get('/:id', async (req, res) => {
+    try {
+        const hotel = await Hotel.findById(req.params.id)
+        res.status(200).json(hotel); 
+    } catch (error) {
+        res.status(500).json(error) 
+    }
+})
+
+// get all
+router.get('/', async (req, res) => {
+    try {
+        const hotels = await Hotel.find()
+        res.status(200).json(hotels); 
+    } catch (error) {
+        res.status(500).json(error) 
+    }
+})
+
+
 export default router;
